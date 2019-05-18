e4aa3dc562708843bdfdbd23f2348ab1cfab870a
Nitish Bahl <nitishbahl24@gmail.com>
2019-05-19 02:29:19 +0530

    basic setup

---
+++ administrator/components/com_templates/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">Page Builder</div>
+    <hr>
+
+    <div class="form-group">
+      <button class="btn btn-primary">Add</button>
+      <button class="btn btn-primary">Remove</button>
+    </div>
+
+    <draggable :list="list"></draggable>
+
+  </div>
+</template>
+
+<script>
+  
+
+  export default {
+
+  }
+</script>
+
+<style>
+  button {
+    margin: 0.1em
+  }
+</style>
---
+++ administrator/components/com_templates/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import draggable from 'vuedraggable'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
