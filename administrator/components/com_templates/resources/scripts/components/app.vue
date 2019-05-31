1d60d9d2db793714c239c1d3bb9b64ece270ba04
Franciska Perisa <franci_perisa@hotmail.de>
2019-05-31 18:41:15 +0200

    Bring back overwritten form and grid prop

--- administrator/components/com_templates/resources/scripts/components/app.vue
+++ administrator/components/com_templates/resources/scripts/components/app.vue
@@ -1,67 +1,78 @@
 <template>
 <div class="container-fluid row">
-  <div id="Settings" class="col-sm-2">
+  <div id="Settings" class="settings col-sm-2">
       <h2>Settings</h2>
       <hr>
+      <form>
+        <div v-if="edit_position">
+            <fieldset>
+                <legend>Edit module position - {{ selected_pos }}</legend>
+                <div class="form-group">
+                    <label for="module_chrome">Select module chrome</label>
+                    <select id="module_chrome" name="module_chrome" v-model="module_chrome">
+                      <option value="none">none</option>
+                      <option value="rounded">rounded</option>
+                      <option value="table">table</option>
+                      <option value="horz">horz</option>
+                      <option value="xhtml">xhtml</option>
+                      <option value="html5">html5</option>
+                      <option value="outline">outline</option>
+                    </select>
+                </div>
 
-      <div v-if="edit_position">
-        <strong>Edit module position - {{ selected_pos }}</strong>
-        <br>
-        <label for="module_chrome">Select module chrome</label>
-        <br>
-        <select id="module_chrome" name="module_chrome" v-model="module_chrome">
-          <option value="none">none</option>
-          <option value="rounded">rounded</option>
-          <option value="table">table</option>
-          <option value="horz">horz</option>
-          <option value="xhtml">xhtml</option>
-          <option value="html5">html5</option>
-          <option value="outline">outline</option>
-        </select>
-        <br>
+                <div class="form-group">
+                  <label for="size_input">Select module size</label>
+                  <input id="size_input" name="size_input" type="text" v-model="size_input" class="form-control">
+                </div>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-success" @click="save">Save</button>
+                    <button type="button" class="btn btn-secondary" @click="back">Back</button>
+                </div>
+            </fieldset>
+        </div>
 
-        <label for="size_input">Select module size</label>
-        <input id="size_input" name="size_input" type="text" v-model="size_input">
+        <div v-else-if="edit_column" class="btn-group">
+            <button type="button" class="btn btn-success" @click="">Save</button>
+            <button type="button" class="btn btn-secondary" @click="">Back</button>
+        </div>
 
-        <button type="button" class="btn btn-success" @click="save">Save</button>
-        <button type="button" class="btn btn-secondary" @click="back">Back</button>
-      </div>
+      <div v-else-if="edit_grid" class="form-group">
+        <fieldset>
+            <legend>Edit Grid</legend>
+            <label for="column_size">Add new column</label>
+            <input id="column_size" name="column_size" type="text" v-model="column_size">
 
-      <div v-else-if="edit_column">
-        <button type="button" class="btn btn-success" @click="">Save</button>
-        <button type="button" class="btn btn-secondary" @click="back">Back</button>
-      </div>
-
-      <div v-else-if="edit_grid">
-        <strong>Edit Grid</strong>
-        <br>
-        <label for="column_size">Add new column</label>
-        <input id="column_size" name="column_size" type="text" v-model="column_size">
-        <br>
-        <button type="button" class="btn btn-success" @click="editGrid('',true)">Save</button>
-        <button type="button" class="btn btn-danger" @click="back">Back</button>
+            <div class="btn-group">
+                <button type="button" class="btn btn-success" @click="editGrid('',true)">Save</button>
+                <button type="button" class="btn btn-danger" @click="back">Back</button>
+            </div>
+        </fieldset>
       </div>
 
       <div v-else>
-        <strong>Add new grid</strong>
-        <br>
-        <label for="grid_type">Select grid system(should add to 12)</label>
-        <input id="grid_type" name="grid_type" type="text" v-model="grid_system">
-        <br>
-        <button type="button" class="btn btn-primary" @click="addGrid">Add</button>
-        <button type="button" class="btn btn-secondary" @click="grid_system = ''">Reset</button>
+          <fieldset>
+            <legend>Add new grid</legend>
+            <label for="grid_type">Select grid system(should add to 12)</label>
+            <input id="grid_type" name="grid_type" type="text" v-model="grid_system">
+
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary" @click="addGrid">Add</button>
+                <button type="button" class="btn btn-secondary" @click="grid_system = ''">Reset</button>
+            </div>
+          </fieldset>
       </div>
+    </form>
   </div>
 
   <div id="View" class="col-sm-10">
     <h2>View</h2>
-    <hr>
     <draggable v-model="gridArray">
       <div v-for="grid in gridArray" v-model="gridArray" class="draggable">
         <draggable v-model="grid.children" class="row grid-row">
           <div class="list-group-item" v-for="column in grid.children" :class="[column.options.size]" @click="">
             {{column.options.size}}
-            (<i>{{column.type}}</i>)<button type="button" class="remove" @click="deleteColumn(grid,column)">&times;</button>
+            (<i>{{column.type}}</i>)
+            <button type="button" class="close" aria-label="Remove" @click="deleteColumn(grid,column)">&times;</button>
           </div>
         </draggable>
         <button type="button" class="btn-primary btn editGrid" @click="editGrid(grid,false)">Edit Grid</button>
@@ -69,7 +80,7 @@
       </div>
     </draggable>
     <button type="button" class="btn btn-outline-info btn-block" data-toggle="modal" data-target="#newgrid">+</button>
-    
+
     <!-- Modal -->
     <div class="modal fade" id="newgrid" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
@@ -109,6 +120,56 @@
 <script>
   import draggable from 'vuedraggable'
   export default {
+    props: {
+      grid: {
+        type: Array,
+        required: false,
+        default: function () {
+          return [
+            {
+              type: 'grid',
+              options: {},
+              children:
+                [
+                  {
+                    type: 'column',
+                    options: {
+                      size: 'col-sm-4'
+                    },
+                    children: [{
+                      type: 'position',
+                      options: {},
+                      children: []
+                    }]
+                  },
+                  {
+                    type: 'column',
+                    options: {
+                      size: 'col-sm-4'
+                    },
+                    children: [{
+                      type: 'position',
+                      options: {},
+                      children: []
+                    }]
+                  },
+                  {
+                    type: 'column',
+                    options: {
+                      size: 'col-sm-4'
+                    },
+                    children: [{
+                      type: 'position',
+                      options: {},
+                      children: []
+                    }]
+                  }
+                ]
+            }
+          ];
+        },
+      }
+    },
     data() {
       return {
         myArray: [],
@@ -123,50 +184,16 @@
         grid_system: '',
         grid_selected: '',
         column_size: '',
-        gridArray: 
-        [
-          {
-            type: 'grid',
-            options: {},
-            children: 
-            [
-              {
-                type: 'column',
-                options: {
-                  size: 'col-sm-4'
-                },
-                children: [{
-                  type: 'position',
-                  options: {},
-                  children: []
-                }]
-              },
-              {
-                type: 'column',
-                options: {
-                  size: 'col-sm-4'
-                },
-                children: [{
-                  type: 'position',
-                  options: {},
-                  children: []
-                }]
-              },
-              {
-                type: 'column',
-                options: {
-                  size: 'col-sm-4'
-                },
-                children: [{
-                  type: 'position',
-                  options: {},
-                  children: []
-                }]
-              }
-            ]
-          }
-        ]
+        gridArray: this.grid,
       }
+    },
+    watch: {
+      grid: {
+        handler: function(newVal, oldVal) {
+          document.getElementById('jform_params_grid').value = JSON.stringify(newVal);
+        },
+        deep: true,
+      },
     },
     components: {
       draggable
@@ -174,7 +201,7 @@
     methods: {
       addGrid() {
         var gridSize = this.grid_system.split(" ");
-        this.myArray = []
+        this.myArray = [];
         gridSize.forEach(element => {
           this.myArray.push({
             type: 'column',
@@ -192,9 +219,8 @@
           type: 'grid',
           options: {},
           children: this.myArray
-        })
+        });
         this.grid_system = '';
-        console.log(this.gridArray);
       },
       deleteGrid(grid) {
         var index = this.gridArray.indexOf(grid);
@@ -209,7 +235,6 @@
       },
       editColumn(element,grid) {
         this.edit_column = true;
-        
       },
       editGrid(grid,submit) {
         if(submit){
@@ -223,7 +248,7 @@
               options: {},
               children: []
             }]
-          })
+          });
           this.grid_selected = '';
           this.column_size = '';
           this.edit_grid = false;
