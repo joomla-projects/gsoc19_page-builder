d886ddc251bdcfa9eab0b1103481ac4dce3bb061
bahl24 <nitishbahl24@gmail.com>
2019-06-06 00:48:14 +0530

    Removed rawDisplayer file ( not req)

--- administrator/components/com_templates/resources/scripts/pagebuilder.js
+++ administrator/components/com_templates/resources/scripts/pagebuilder.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 import App from './components/app.vue';
-import rawDisplayer from './components/raw-displayer.vue';
 
 Vue.config.productionTip = false;
-Vue.component("raw-displayer", rawDisplayer);
 
 document.addEventListener("DOMContentLoaded",
   () => new Vue({
