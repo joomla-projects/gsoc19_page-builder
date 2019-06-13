f4b118ed333dc81af760d6957886b59957f2e2b0
Franciska Perisa <franci_perisa@hotmail.de>
2019-06-13 02:16:39 +0200

    Fix appearance of add grid modal

--- administrator/components/com_templates/resources/scripts/components/modals/modal-add-grid.vue
+++ administrator/components/com_templates/resources/scripts/components/modals/modal-add-grid.vue
@@ -1,31 +1,31 @@
 <template>
-	<modal name="add-grid" role="dialog" :classes="['modal-content', 'v--modal']">
-			<div class="modal-header">
-				<h5 class="modal-title">{{ translate('COM_TEMPLATES_SELECT_LAYOUT') }}</h5>
-				<button @click="close" type="button" class="close" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
+	<modal name="add-grid" role="dialog" :classes="['modal-content', 'v--modal']" height="auto">
+		<div class="modal-header">
+			<h5 class="modal-title">{{ translate('COM_TEMPLATES_SELECT_LAYOUT') }}</h5>
+			<button @click="close" type="button" class="close" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		<div class="modal-body">
+			<h6>{{ translate('COM_TEMPLATES_PREDEFINED') }}</h6>
+			<div class="row image-selection">
+				<div class="col" v-html="images.row12" @click="$emit('selection', [12])"></div>
+				<div class="col" v-html="images.row66" @click="$emit('selection', [6, 6])"></div>
+				<div class="col" v-html="images.row48" @click="$emit('selection', [4, 8])"></div>
+				<div class="col" v-html="images.row84" @click="$emit('selection', [8, 4])"></div>
+				<div class="col" v-html="images.row3333" @click="$emit('selection', [3, 3, 3, 3])"></div>
+				<div class="col" v-html="images.row444" @click="$emit('selection', [4, 4, 4])"></div>
+				<div class="col" v-html="images.row363" @click="$emit('selection', [3, 6, 3])"></div>
 			</div>
-			<div class="modal-body">
-                {{ translate('COM_TEMPLATES_PREDEFINED') }}
-                <div class="row">
-                    <div class="col" v-html="images.row12" v-on:click="$emit('selection', [12])"></div>
-                    <div class="col" v-html="images.row66" v-on:click="$emit('selection', [6, 6])"></div>
-                    <div class="col" v-html="images.row48" v-on:click="$emit('selection', [4, 8])"></div>
-                    <div class="col" v-html="images.row84" v-on:click="$emit('selection', [8, 4])"></div>
-                    <div class="col" v-html="images.row3333" v-on:click="$emit('selection', [3, 3, 3, 3])"></div>
-                    <div class="col" v-html="images.row444" v-on:click="$emit('selection', [4, 4, 4])"></div>
-                    <div class="col" v-html="images.row363" v-on:click="$emit('selection', [3, 6, 3])"></div>
-			    </div>
-                <div>	
-                    <label>{{ translate('COM_TEMPLATES_CUSTOM') }}</label>	
-                    <input name="column_size" type="text" v-model="grid_system">
-                </div>
+			<div class="form-group">
+				<label for="custom">{{ translate('COM_TEMPLATES_CUSTOM') }}</label>
+				<input id="custom" name="custom" type="text" v-model="grid_system" class="form-control">
 			</div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger mr-auto" data-dismiss="modal">{{ translate('COM_TEMPLATES_CLOSE') }}</button>	
-                <button type="button" class="btn btn-success" @click="submit" data-dismiss="modal">{{ translate('COM_TEMPLATES_ADD') }}</button>
-            </div>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-danger mr-auto" @click="close">{{ translate('COM_TEMPLATES_CLOSE') }}</button>
+			<button type="button" class="btn btn-success" @click="submit">{{ translate('COM_TEMPLATES_ADD') }}</button>
+		</div>
 	</modal>
 </template>
 
@@ -33,34 +33,34 @@
   export default {
     name: 'modal-add-grid',
     data() {
-        return {
-            grid_system: '',
-        }
+      return {
+        grid_system: '',
+      };
     },
     props: {
       joptions: {
         type: Object,
         required: false,
         default: function () {
-            return window.Joomla.getOptions('com_templates');
+          return window.Joomla.getOptions('com_templates');
         },
       },
       images: {
         type: Object,
         required: false,
         default: function () {
-            return this.joptions.images;
+          return this.joptions.images;
         },
       },
     },
     methods: {
-        close() {
-            this.$modal.hide('add-grid');
-        },
-        submit() {
-            this.$emit('selection', this.grid_system.split(' '));
-            this.$modal.hide('add-grid');
-        }
+      close() {
+        this.$modal.hide('add-grid');
+      },
+      submit() {
+        this.$emit('selection', this.grid_system.split(' '));
+        this.$modal.hide('add-grid');
+      }
     },
   };
 </script>
