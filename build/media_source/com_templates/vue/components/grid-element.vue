185e5aa8528f9c4548ea7dfefb0ce7203d861c36
Franciska Perisa <franci_perisa@hotmail.de>
2019-06-25 14:29:00 +0200

    Insert add-column button as static grid element

--- build/media_source/com_templates/vue/components/grid-element.vue
+++ build/media_source/com_templates/vue/components/grid-element.vue
@@ -5,6 +5,7 @@
 				 :is-draggable="true"
 				 :is-resizable="false"
 	>
+		<!-- TODO: find a better way to show these buttons -->
 		<div class="btn-wrapper">
 			<button type="button" class="btn btn-lg" @click="$emit('editGrid', grid)">
 				<span class="icon-options"></span>
@@ -15,7 +16,6 @@
 				<span class="sr-only">{{ translate('COM_TEMPLATES_DELETE_GRID') }}</span>
 			</button>
 		</div>
-		<span v-if="grid.options">.{{grid.options.class}}</span>
 
 		<grid-item v-for="column in grid.children" :key="column.i" class="col-wrapper"
 				   :is-resizable="true"
@@ -24,9 +24,7 @@
 				   :h="column.h"
 				   :x="column.x"
 				   :y="column.y"
-				   @resize="changeSize"
-				   @resized="resizeEnd"
-			>
+		>
 			<div class="btn-wrapper">
 				<button type="button" class="btn btn-lg" @click="$emit('editColumn')">
 					<span class="icon-options"></span>
@@ -37,17 +35,29 @@
 					<span class="sr-only">{{ translate('COM_TEMPLATES_DELETE_COLUMN') }}</span>
 				</button>
 			</div>
-			<span>{{column.size}} (<i>{{column.type}}<span v-if="column.options.class">, .{{column.options.class}}</span></i>)</span>
+			<span>{{column.w}} (<i>{{column.type}}<span
+					v-if="column.options.class">, .{{column.options.class}}</span></i>)</span>
 			<button type="button" class="btn btn-add btn-outline-info" @click="$emit('addElement', column)">
 				<span class="icon-new"></span>
 				{{ translate('COM_TEMPLATES_ADD_ELEMENT') }}
 			</button>
 		</grid-item>
 
-		<button class="btn btn-add btn-outline-info" type="button" @click="$emit('addColumn', grid)">
-			<span class="icon-new"></span>
-			{{ translate('COM_TEMPLATES_ADD_COLUMN') }}
-		</button>
+		<!-- Button to add new elements into the grid -->
+		<grid-item
+				   :i="addElementIndex"
+				   :is-draggable="false"
+				   :static="true"
+				   :w="1"
+				   :h="1"
+				   :x="nextFreePosition.x"
+				   :y="nextFreePosition.y"
+		>
+			<button class="btn btn-outline-info" type="button" @click="$emit('addColumn', grid)">
+				<span class="icon-new"></span>
+				{{ translate('COM_TEMPLATES_ADD_COLUMN') }}
+			</button>
+		</grid-item>
 	</grid-layout>
 </template>
 
@@ -66,17 +76,30 @@
         type: Number,
       }
     },
+    computed: {
+	  nextFreePosition: function() {
+        let nextPosition = 0;
+        let rowNumber = 0;
+
+        this.grid.children.forEach(child => {
+          nextPosition += child.w;
+
+          if (nextPosition === this.gridSize) {
+            nextPosition = 0;
+            rowNumber += 1;
+          }
+        });
+        return {x: nextPosition, y: rowNumber};
+	  }
+    },
+    data() {
+      return {
+        addElementIndex: 'add',
+      };
+    },
     components: {
       GridLayout: VueGridLayout.GridLayout,
       GridItem: VueGridLayout.GridItem,
     },
-    methods: {
-      resizeEnd(left, top, width, height) {
-        // TODO: change column.options.size
-      },
-      changeSize(left, top, width, height) {
-
-      }
-    }
   };
 </script>
