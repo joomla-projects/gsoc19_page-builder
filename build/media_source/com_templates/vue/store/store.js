05c78d6d32840665f26252e7ced08da085f45192
Franciska Perisa <franci_perisa@hotmail.de>
2019-07-03 21:38:58 +0200

    Refactor vuex store and remove 'col-' from size

--- build/media_source/com_templates/vue/store/store.js
+++ build/media_source/com_templates/vue/store/store.js
@@ -1,165 +1,146 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
 const state = {
-    sizeArray: [],
-    gridSelected: '',
-    columnSelected: '',
-    selectedSettings: '',
-    parent: '',
-    allowedChildren: [],
-    childAllowed: [],
-    elements: window.Joomla.getOptions('com_templates').elements,
-    elementArray: {}
+  gridSelected: '',
+  columnSelected: '',
+  selectedSettings: '',
+  parent: '',
+  allowedChildren: [],
+  childAllowed: [],
+  elements: window.Joomla.getOptions('com_templates').elements,
+  elementArray: {}
 };
 
-const actions = {
-
-}
+const actions = {};
 
 const mutations = {
-    mapGrid(state, payload) {
-        state.elementArray = payload;
-    },
-    ifChildAllowed(state) {
-        state.elements.forEach(el => {
-            if(el.children)
-                state.childAllowed.push(el.name);
-        })
-    },
-    addGrid(state, sizes) {
-        state.sizeArray = [];
-        sizes.forEach(size => {
-            state.sizeArray.push({
-                type: 'Column',
-                options: {
-                size: 'col-' + size,
-                class: ''
-                },
-                children: []
-            });
-        });
-        if(state.parent.children) {
-            state.parent.children.push({
-                type: 'Grid',
-                options: {
-                class: '',
-                },
-                children: state.sizeArray
-            });
-        }
-        else {
-            state.parent.push({
-                type: 'Grid',
-                options: {
-                  class: '',
-                },
-                children: state.sizeArray
-            });
-        }
-    },
-    deleteElement(state, element) {
-        const index = state.elementArray.indexOf(element);
-        if (index > -1)
-        state.elementArray.splice(index, 1);
-    },
-    addColumn(state, grid) {
-        state.gridSelected = grid;
-        state.selectedSettings = 'add-column';
-    },
-    editColumn(state, column) {
-        document.getElementById("sidebar").style.width = "250px";
-        document.getElementById("pagebuilder").style.marginLeft = "250px";
-        state.columnSelected = column;
-        state.selectedSettings = 'edit-column';
-    },
-    editElement(state, element) {
-        document.getElementById("sidebar").style.width = "250px";
-        document.getElementById("pagebuilder").style.marginLeft = "250px";
-        state.selectedSettings = 'edit-grid';
-        state.gridSelected = element;
-    },
-    closeNav(state) {
-        document.getElementById("sidebar").style.width = "0";
-        document.getElementById("pagebuilder").style.marginLeft = "0";
-    },
-    addContainer(state) {
-        if(state.parent.children) {
-        state.parent.children.push({
-            type: 'Container',
-            options: {
-            class: ''
-            },
-            children: []
-        })
-        }
-        else {
-        state.parent.push({
-            type: 'Container',
-            options: {
-            class: ''
-            },
-            children: []
-        })
-        }
-    },
-    fillAllowedChildren(state, name) {
-        state.allowedChildren = [];
+  mapGrid(state, payload) {
+    state.elementArray = payload;
+  },
+  ifChildAllowed(state) {
+    state.elements.forEach(el => {
+      if (el.children) {
+        state.childAllowed.push(el.name);
+      }
+    });
+  },
+  addGrid(state, sizes) {
+    const newElements = [];
 
-        // Check if parent is root
-        if(name == undefined)
-        name = 'root';
-        state.elements.forEach(el => {
-        el.parent.forEach(item => {
-            if(item == name)
-            state.allowedChildren.push({
-                'name': el.name,
-                'description': el.description
-            });
-        })
-        })
-    },
-    addElement(state, parent) {
-        state.parent = parent;
-    },
-    deleteColumn(state, payload) {
-        console.log(payload.element);
-        const index = payload.element.children.indexOf(payload.column);
-        if (index > -1) {
-            payload.element.children.splice(index, 1);
-        }
-    },
-    insertElem(state, element) {
-        if(state.parent.children) {
-            state.parent.children.push({
-                type: element,
-                options: {
-                    class: ''
-                },
-                children: []
-            })
-        }
-        else {
-            state.parent.push({
-                type: element,
-                options: {
-                    class: ''
-                },
-                children: []
-            })
-        }
+    sizes.forEach(size => {
+      newElements.push({
+        type: 'Column',
+        options: {
+          size: size,
+          class: ''
+        },
+        children: []
+      });
+    });
+
+    const newGrid = {
+      type: 'Grid',
+      options: {
+        class: '',
+      },
+      children: newElements
+    };
+
+    if (state.parent.children) {
+      state.parent.children.push(newGrid);
+    } else {
+      state.parent.push(newGrid);
     }
-}
+  },
+  deleteElement(state, element) {
+    const index = state.elementArray.indexOf(element);
+    if (index > -1) {
+      state.elementArray.splice(index, 1);
+    }
+  },
+  addColumn(state, grid) {
+    state.gridSelected = grid;
+    state.selectedSettings = 'add-column';
+  },
+  editElement(state, element) {
+    mutations.openNav();
+    state.selectedSettings = 'edit-grid';
+    state.gridSelected = element;
+  },
+  closeNav() {
+    document.getElementById('sidebar').style.width = '0';
+    document.getElementById('pagebuilder').style.marginLeft = '0';
+  },
+  openNav() {
+    document.getElementById('sidebar').style.width = '250px';
+    document.getElementById('pagebuilder').style.marginLeft = '250px';
+  },
+  addContainer(state) {
+    const newContainer = {
+      type: 'Container',
+      options: {
+        class: ''
+      },
+      children: []
+    };
+    if (state.parent.children) {
+      state.parent.children.push(newContainer);
+    } else {
+      state.parent.push(newContainer);
+    }
+  },
+  fillAllowedChildren(state, name) {
+    state.allowedChildren = [];
 
-const getters = {
+    // Check if parent is root
+    if (!name) {
+      name = 'root';
+    }
 
-}
+    state.elements.forEach(el => {
+      el.parent.forEach(item => {
+        if (item === name) {
+          state.allowedChildren.push({
+            'name': el.name,
+            'description': el.description
+          });
+        }
+      });
+    });
+  },
+  addElement(state, parent) {
+    state.parent = parent;
+  },
+  deleteColumn(state, payload) {
+    const index = payload.element.children.indexOf(payload.column);
+    if (index > -1) {
+      payload.element.children.splice(index, 1);
+    }
+  },
+  insertElem(state, element) {
+    const newElement = {
+      type: element,
+      options: {
+        class: ''
+      },
+      children: []
+    };
+    if (state.parent.children) {
+      state.parent.children.push(newElement);
+    } else {
+      state.parent.push(newElement);
+    }
+  }
+};
+
+const getters = {};
 
 export default new Vuex.Store({
-    state,
-    getters,
-    actions,
-    mutations,
-})
+  state,
+  getters,
+  actions,
+  mutations,
+});
