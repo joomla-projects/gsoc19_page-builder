dd2fe1f15ecf129b8188c78b4766ebc6cce85279
bahl24 <nitishbahl24@gmail.com>
2019-07-12 03:58:11 +0530

    fix phpcs

--- plugins/pagebuilder/moduleposition/moduleposition.php
+++ plugins/pagebuilder/moduleposition/moduleposition.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * @package    Joomla.Plugin
- * @subpackage Page Builder
  *
  * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
