63fdc9a176547e733a7ac0bb131477891c018a8a
Franciska Perisa <franci_perisa@hotmail.de>
2019-07-11 22:28:55 +0200

    Fix selection of element without row selection

--- build/media_source/com_templates/vue/components/modals/modal-add-element.vue
+++ build/media_source/com_templates/vue/components/modals/modal-add-element.vue
@@ -11,33 +11,32 @@
 			<div class="row">
 				<div class="col-4">
 					<div class="list-group" id="list-tab" role="tablist">
-						<li v-for="element in allowedChildren" class="list-group-item list-group-item-action" data-toggle="list"
-							:href="'#list-'+element.name" role="tabpanel">
+						<li v-for="element in allowedChildren" data-toggle="list" role="tabpanel"
+							class="element-selection list-group-item list-group-item-action"
+							:href="'#list-'+element.name">
 							{{ element.name }}
 						</li>
 					</div>
 				</div>
-				<div class="col-8">
-					<div class="tab-content">
-						<div v-for="element in allowedChildren" :id="'list-'+element.name" class="tab-pane fade" role="tabpanel">
-							{{ element.description }}
-							<div v-if="element.name === 'Grid'" class="image-selection">
-								<div class="row">
-									<div class="col-4 icon" v-html="images.row12 + '<p>100%</p>'"
-										@click="insertElement('Grid' , [12])"></div>
-									<div class="col-4 icon" v-html="images.row66 + '<p>(50%-50%)</p>'"
-										@click="insertElement('Grid' , [6, 6])"></div>
-									<div class="col-4 icon" v-html="images.row48 + '<p>(33%-67%)</p>'"
-										@click="insertElement('Grid' , [4, 8])"></div>
-									<div class="col-4 icon" v-html="images.row84 + '<p>(67%-33%)</p>'"
-										@click="insertElement('Grid' , [8, 4])"></div>
-									<div class="col-4 icon" v-html="images.row3333 + '<p>(25%-25%-25%-25%)</p>'"
-										@click="insertElement('Grid' , [3, 3, 3, 3])"></div>
-									<div class="col-4 icon" v-html="images.row444 + '<p>(33%-33%-33%)</p>'"
-										@click="insertElement('Grid' , [4, 4, 4])"></div>
-									<div class="col-4 icon" v-html="images.row363 + '<p>(25%-50%-25%)</p>'"
-										@click="insertElement('Grid' , [3, 6, 3])"></div>
-								</div>
+				<div class="col-8 tab-content">
+					<div v-for="element in allowedChildren" :id="'list-'+element.name" class="tab-pane fade" role="tabpanel">
+						{{ element.description }}
+						<div v-if="element.name === 'Grid'" class="image-selection">
+							<div class="row">
+								<div class="col-4 icon" v-html="images.row12 + '<p>100%</p>'"
+									@click="insertElement('Grid' , [12])"></div>
+								<div class="col-4 icon" v-html="images.row66 + '<p>(50%-50%)</p>'"
+									@click="insertElement('Grid' , [6, 6])"></div>
+								<div class="col-4 icon" v-html="images.row48 + '<p>(33%-67%)</p>'"
+									@click="insertElement('Grid' , [4, 8])"></div>
+								<div class="col-4 icon" v-html="images.row84 + '<p>(67%-33%)</p>'"
+									@click="insertElement('Grid' , [8, 4])"></div>
+								<div class="col-4 icon" v-html="images.row3333 + '<p>(25%-25%-25%-25%)</p>'"
+									@click="insertElement('Grid' , [3, 3, 3, 3])"></div>
+								<div class="col-4 icon" v-html="images.row444 + '<p>(33%-33%-33%)</p>'"
+									@click="insertElement('Grid' , [4, 4, 4])"></div>
+								<div class="col-4 icon" v-html="images.row363 + '<p>(25%-50%-25%)</p>'"
+									@click="insertElement('Grid' , [3, 6, 3])"></div>
 							</div>
 						</div>
 					</div>
@@ -46,7 +45,7 @@
 			<small class="form-text text-muted">{{ translate('COM_TEMPLATES_ADD_ELEMENT_DESC') }}</small>
 		</div>
 		<div class="modal-footer">
-			<button type="button" class="btn btn-success mr-2" @click="add">
+			<button type="button" class="btn btn-success" @click="add">
 				{{ translate('COM_TEMPLATES_SAVE') }}
 			</button>
 			<button type="button" class="btn btn-danger" @click="$modal.hide('add-element')">
@@ -69,18 +68,17 @@
     computed: {
       ...mapState([
         'elements',
-        'allowedChildren',
+        'allowedChildren'
       ])
     },
     methods: {
       ...mapMutations([
-        'addGrid',
-        'addContainer',
+        'addGrid'
       ]),
       add() {
-        const selection = document.querySelector('a.active').innerText;
+        const selection = document.querySelector('.element-selection.active');
         if (selection) {
-          this.insertElement(selection);
+          this.insertElement(selection.innerText);
           this.$modal.hide('add-element');
         }
       },
