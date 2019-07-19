6864b44da93b81842249856397689b42b5bbfff2
bahl24 <nitishbahl24@gmail.com>
2019-07-19 10:52:51 +0530

    append 'pagebuilder_' to class name

--- build/media_source/com_templates/vue/components/elements/item.vue
+++ build/media_source/com_templates/vue/components/elements/item.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :class="['item', element.type, element.options.class]">
+	<div :class="['item', 'pagebuilder_' + element.type, element.options.class]">
 		<div class="btn-wrapper btn-group">
 			<button type="button" class="btn btn-primary btn-sm" @click="editElement(element)">
 				<span class="icon-options"></span>
