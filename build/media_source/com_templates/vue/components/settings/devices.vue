2b8ae7b1ee7148c35cc515207f3aa76c5921f8b2
Franciska Perisa <franci_perisa@hotmail.de>
2019-08-08 20:48:28 +0200

    Set selected device like stored activeDevice

--- build/media_source/com_templates/vue/components/settings/devices.vue
+++ build/media_source/com_templates/vue/components/settings/devices.vue
@@ -4,31 +4,31 @@
 			<span class="nav-link">{{ translate('COM_TEMPLATES_SELECT_DEVICE')}}</span>
 		</li>
 		<li class="nav-item">
-			<button @click="setDevice('xs', $event)" class="btn btn-info" type="button">
+			<button @click="setDevice('xs', $event)" id="xs" class="btn btn-info" type="button">
 				<span class="fas fa-mobile-alt fa-lg" aria-hidden="true"></span>
 				{{ translate('COM_TEMPLATES_DEVICE_XS') }}
 			</button>
 		</li>
 		<li class="nav-item">
-			<button @click="setDevice('sm', $event)" class="btn btn-info active" type="button">
+			<button @click="setDevice('sm', $event)" id="sm" class="btn btn-info" type="button">
 				<span class="fas fa-tablet-alt fa-lg" aria-hidden="true"></span>
 				{{ translate('COM_TEMPLATES_DEVICE_SM') }}
 			</button>
 		</li>
 		<li class="nav-item">
-			<button @click="setDevice('md', $event)" class="btn btn-info" type="button">
+			<button @click="setDevice('md', $event)" id="md" class="btn btn-info" type="button">
 				<span class="fas fa-tablet-alt fa-lg fa-rotate-270" aria-hidden="true"></span>
 				{{ translate('COM_TEMPLATES_DEVICE_MD') }}
 			</button>
 		</li>
 		<li class="nav-item">
-			<button @click="setDevice('lg', $event)" class="btn btn-info" type="button">
+			<button @click="setDevice('lg', $event)" id="lg" class="btn btn-info" type="button">
 				<span class="fas fa-laptop fa-lg" aria-hidden="true"></span>
 				{{ translate('COM_TEMPLATES_DEVICE_LG') }}
 			</button>
 		</li>
 		<li class="nav-item">
-			<button @click="setDevice('xl', $event)" class="btn btn-info" type="button">
+			<button @click="setDevice('xl', $event)" id="xl" class="btn btn-info" type="button">
 				<span class="fas fa-desktop fa-lg" aria-hidden="true"></span>
 				{{ translate('COM_TEMPLATES_DEVICE_XL') }}
 			</button>
@@ -43,8 +43,11 @@
     name: 'devices',
     computed: {
       ...mapState([
-        'resolution'
+        'activeDevice'
       ]),
+    },
+    mounted() {
+      document.querySelector(`#devices #${this.activeDevice}`).classList.add('active');
     },
     methods: {
       ...mapMutations([
@@ -53,7 +56,7 @@
       setDevice(size, event) {
         this.updateDeviceWidth(size);
 
-        const buttons = document.querySelectorAll('#devices > li > button');
+        const buttons = document.querySelectorAll('#devices button');
         buttons.forEach(btn => {
           btn.classList.remove('active');
         });
