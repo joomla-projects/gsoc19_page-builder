57ce5907ebc6060c6447c8ff9f5166c2caa75088
Franciska Perisa <franciska.perisa@mni.thm.de>
2019-08-15 14:50:35 +0200

    Add description for selected device

--- build/media_source/system/js/fields/pagebuilder/components/settings/devices.vue
+++ build/media_source/system/js/fields/pagebuilder/components/settings/devices.vue
@@ -1,37 +1,37 @@
 <template>
 	<ul id="devices" class="nav nav-pills">
 		<li class="nav-item">
-			<span class="nav-link">{{ translate('JLIB_PAGEBUILDER_SELECT_DEVICE')}}</span>
-		</li>
-		<li class="nav-item">
 			<button @click="setDevice('xs', $event)" id="xs" class="btn btn-info" type="button">
 				<span class="fas fa-mobile-alt fa-lg" aria-hidden="true"></span>
-				{{ translate('JLIB_PAGEBUILDER_DEVICE_XS') }}
+				<span class="sr-only">{{ translate('JLIB_PAGEBUILDER_DEVICE_XS_SR_DESC') }}</span>
 			</button>
 		</li>
 		<li class="nav-item">
 			<button @click="setDevice('sm', $event)" id="sm" class="btn btn-info" type="button">
 				<span class="fas fa-tablet-alt fa-lg" aria-hidden="true"></span>
-				{{ translate('JLIB_PAGEBUILDER_DEVICE_SM') }}
+				<span class="sr-only">{{ translate('JLIB_PAGEBUILDER_DEVICE_SM_SR_DESC') }}</span>
 			</button>
 		</li>
 		<li class="nav-item">
 			<button @click="setDevice('md', $event)" id="md" class="btn btn-info" type="button">
 				<span class="fas fa-tablet-alt fa-lg fa-rotate-270" aria-hidden="true"></span>
-				{{ translate('JLIB_PAGEBUILDER_DEVICE_MD') }}
+				<span class="sr-only">{{ translate('JLIB_PAGEBUILDER_DEVICE_MD_SR_DESC') }}</span>
 			</button>
 		</li>
 		<li class="nav-item">
 			<button @click="setDevice('lg', $event)" id="lg" class="btn btn-info" type="button">
 				<span class="fas fa-laptop fa-lg" aria-hidden="true"></span>
-				{{ translate('JLIB_PAGEBUILDER_DEVICE_LG') }}
+				<span class="sr-only">{{ translate('JLIB_PAGEBUILDER_DEVICE_LG_SR_DESC') }}</span>
 			</button>
 		</li>
 		<li class="nav-item">
 			<button @click="setDevice('xl', $event)" id="xl" class="btn btn-info" type="button">
 				<span class="fas fa-desktop fa-lg" aria-hidden="true"></span>
-				{{ translate('JLIB_PAGEBUILDER_DEVICE_XL') }}
+				<span class="sr-only">{{ translate('JLIB_PAGEBUILDER_DEVICE_XL_SR_DESC') }}</span>
 			</button>
+		</li>
+		<li class="nav-item">
+			<span id="device-desc" class="nav-link"></span>
 		</li>
 	</ul>
 </template>
@@ -43,15 +43,19 @@
     name: 'devices',
     computed: {
       ...mapState([
-        'activeDevice'
+        'activeDevice',
       ]),
+      deviceDesc() {
+        return document.getElementById('device-desc');
+      },
     },
     mounted() {
       document.querySelector(`#devices #${this.activeDevice}`).classList.add('active');
+      this.deviceDesc.innerText = this.translate(`JLIB_PAGEBUILDER_DEVICE_${this.activeDevice.toUpperCase()}`);
     },
     methods: {
       ...mapMutations([
-        'updateDeviceWidth'
+        'updateDeviceWidth',
       ]),
       setDevice(size, event) {
         this.updateDeviceWidth(size);
@@ -60,8 +64,9 @@
         buttons.forEach(btn => {
           btn.classList.remove('active');
         });
-        event.target.classList.add('active');
-      }
-    }
+
+        this.deviceDesc.innerText = this.translate(`JLIB_PAGEBUILDER_DEVICE_${size.toUpperCase()}`);
+      },
+    },
   };
 </script>
