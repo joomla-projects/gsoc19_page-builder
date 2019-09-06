ed0a57b63b155f9b2d9fb2d2b2647bb12194a74c
Allan Karlson <beesforever@gmx.de>
2019-09-06 15:10:01 +0200

    add sidebar styles from Luca

--- build/media_source/system/js/fields/pagebuilder/components/app.vue
+++ build/media_source/system/js/fields/pagebuilder/components/app.vue
@@ -1,11 +1,14 @@
 <template>
-	<div>
+	<div class="pagebuilder_container">
 		<div id="sidebar" class="sidebar">
 			<div class="sidebar-content">
-				<h2>{{ translate('JLIB_PAGEBUILDER_SETTINGS') }}</h2>
-				<button type="button" class="btn btn-lg closebtn" @click="closeNav()">
-					<span class="icon-cancel"></span>
-				</button>
+				<h2>{{ translate('JLIB_PAGEBUILDER_SETTINGS') }}
+
+					<button type="button" class="btn btn-lg closeButton" @click="closeNav()" aria-label="Close">
+						<span class="icon-cancel"></span>
+					</button>
+				</h2>
+
 
 				<component :is="selectedSettings"></component>
 			</div>
@@ -18,20 +21,26 @@
 			<div class="col">
 				<ul class="nav nav-pills">
 					<li class="nav-item" id="placeholder_component">
-						<button type="button" class="nav-link drag" id="drag_component" draggable="true" @dragstart="drag($event)">
+						<button type="button" class="nav-link drag" id="drag_component" draggable="true"
+								@dragstart="drag($event)">
 							<i class="fas fa-file-alt"></i>
 							<span>{{ translate('JLIB_PAGEBUILDER_COMPONENT') }}</span>
 							<i class="fas fa-times-circle icon-remove" @click="restorePosition('component')"></i>
 						</button>
 					</li>
 					<li class="nav-item" id="placeholder_message">
-						<button type="button" class="nav-link drag" id="drag_message" draggable="true" @dragstart="drag($event)">
+						<button type="button" class="nav-link drag" id="drag_message" draggable="true"
+								@dragstart="drag($event)">
 							<i class="fas fa-envelope"></i>
 							<span>{{ translate('JLIB_PAGEBUILDER_MESSAGE') }}</span>
 							<i class="fas fa-times-circle icon-remove" @click="restorePosition('message')"></i>
 						</button>
 					</li>
 				</ul>
+			</div>
+			<div class="col col-12" style="height: 100px">
+				<h2>{{ translate('JLIB_PAGEBUILDER_PREVIEW') }}</h2>
+				<div v-html="renderPreview" :style="previewStyle"></div>
 			</div>
 		</div>
 
@@ -41,9 +50,9 @@
 			<!-- Element -->
 			<draggable v-model="elementArray" handle=".handle">
 				<item v-for="element in elementArray" :key="element.key" :class="['row-wrapper']"
-						:item="element" :handleRequired="true"
-						@delete="deleteElement({ element })"
-						@edit="editElement({ element })"></item>
+					  :item="element" :handleRequired="true"
+					  @delete="deleteElement({ element })"
+					  @edit="editElement({ element })"></item>
 			</draggable>
 			<!-- Element Ends -->
 
@@ -59,84 +68,166 @@
 </template>
 
 <script>
-  import {mapMutations, mapState} from 'vuex';
-  import draggable from 'vuedraggable';
+    import {mapMutations, mapState} from 'vuex';
+    import draggable from 'vuedraggable';
+    import axios from "axios";
 
-  export default {
-    computed: {
-      ...mapState([
-        'activeDevice',
-        'resolution',
-        'selectedSettings',
-      ]),
-      widthStyle() {
-        const deviceOrder = Object.keys(this.resolution);
-        const activeIndex = deviceOrder.indexOf(this.activeDevice);
-        const styles = {
-          'min-width': activeIndex === 0 ? 0 : this.resolution[this.activeDevice],
-          'max-width': activeIndex === deviceOrder.length - 1 ? '100%' : this.resolution[deviceOrder[activeIndex + 1]],
-        };
-
-        let styleString = '';
-        for (const name in styles) {
-          styleString += `${name}: ${styles[name]}; `;
-        }
-
-        return styleString;
-      },
-      elementArray: {
-        get() {
-          return this.$store.state.elementArray;
+    export default {
+        data() {
+            return {
+                renderPreview: ""
+            }
         },
-        set(value) {
-          this.updateElementArray(value);
-        }
-      },
-    },
-    watch: {
-      elementArray: {
-        handler(newVal) {
-          document.getElementById('jform_params_grid').value = JSON.stringify(newVal);
+        computed: {
+            ...mapState([
+                'activeDevice',
+                'resolution',
+                'selectedSettings',
+                'advancedSettings'
+            ]),
+            previewStyle() {
+                this.loadAdvancedSettings()
+                let height = 30;
+
+                this.getChildrenSizeOfPageContent(this.elementArray)
+
+
+                return {
+                    'height': `${height}px`,
+                    'background-color': `${this.advancedSettings.bgColor}`,
+                    'color': `${this.advancedSettings.baseColor}`,
+                }
+            },
+            widthStyle() {
+                const deviceOrder = Object.keys(this.resolution);
+                const activeIndex = deviceOrder.indexOf(this.activeDevice);
+                const styles = {
+                    'min-width': activeIndex === 0 ? 0 : this.resolution[this.activeDevice],
+                    'max-width': activeIndex === deviceOrder.length - 1 ? '100%' : this.resolution[deviceOrder[activeIndex + 1]],
+                };
+
+                let styleString = '';
+                for (const name in styles) {
+                    styleString += `${name}: ${styles[name]}; `;
+                }
+
+                return styleString;
+            },
+            elementArray: {
+                get() {
+                    return this.$store.state.elementArray;
+                },
+                set(value) {
+                    this.updateElementArray(value);
+                }
+            },
         },
-        deep: true,
-      },
-    },
-    created() {
-      this.mapElements(JSON.parse(document.getElementById('jform_params_grid').value));
-      this.checkAllowedElements();
-    },
-    mounted() {
-      if(document.getElementsByClassName('drag_component').length) {
-        let element = document.getElementsByClassName('drag_component')[0];
-        element.appendChild(document.getElementById('drag_component'));
-      }
-      if(document.getElementsByClassName('drag_message').length) {
-        let element = document.getElementsByClassName('drag_message')[0];
-        element.appendChild(document.getElementById('drag_message'));
-      }
-    },
-    methods: {
-      ...mapMutations([
-        'checkAllowedElements',
-        'mapElements',
-        'closeNav',
-        'deleteElement',
-        'setParent',
-        'updateElementArray',
-        'editElement',
-        'updateGrid',
-        'restorePosition'
-      ]),
-      addElement() {
-        this.setParent(this.elementArray);
-        this.$modal.show('add-element');
-      },
-      drag(event) {
-        event.dataTransfer.setData('text', event.target.id);
-      },
-    },
-    components: {
-      draggable
-    }
-  };
+        watch: {
+            elementArray: {
+                handler(newVal) {
+                    document.getElementById('jform_params_grid').value = JSON.stringify(newVal);
+                    this.liveViewPost(JSON.stringify(newVal));
+                },
+                deep: true,
+            },
+        },
+        created() {
+            this.mapElements(JSON.parse(document.getElementById('jform_params_grid').value));
+            this.checkAllowedElements();
+        },
+        mounted() {
+            this.loadAdvancedSettings();
+            if (document.getElementsByClassName('drag_component').length) {
+                let element = document.getElementsByClassName('drag_component')[0];
+                element.appendChild(document.getElementById('drag_component'));
+            }
+            if (document.getElementsByClassName('drag_message').length) {
+                let element = document.getElementsByClassName('drag_message')[0];
+                element.appendChild(document.getElementById('drag_message'));
+            }
+            this.liveViewPost(JSON.stringify(this.elementArray));
+        },
+        methods: {
+            ...mapMutations([
+                'checkAllowedElements',
+                'mapElements',
+                'closeNav',
+                'deleteElement',
+                'setParent',
+                'updateElementArray',
+                'editElement',
+                'updateGrid',
+                'restorePosition',
+                'setAdvancedSettings'
+            ]),
+            addElement() {
+                this.setParent(this.elementArray);
+                this.$modal.show('add-element');
+            },
+            drag(event) {
+                event.dataTransfer.setData('text', event.target.id);
+            },
+            getChildrenSizeOfPageContent(rootPoint) {
+
+            },
+            loadAdvancedSettings() {
+                this.setAdvancedSettings({
+                    bgColor: document.getElementById('jform_params_baseBG').value,
+                    baseColor: document.getElementById('jform_params_baseColor').value,
+                    linkColor: document.getElementById('jform_params_linkColor').value
+                })
+            },
+            liveViewPost(data) {
+                let dataConf = {
+                    task: 'ajax.fetchAssociations',
+                    format: 'json',
+                    data: window.btoa(data),
+                    baseBG: `${this.advancedSettings.bgColor}`,
+                    baseColor: `${this.advancedSettings.baseColor}`,
+                    linkColor: `${this.advancedSettings.linkColor}`,
+                    action: 'pagebuilder_liveview'
+                };
+                const queryString = Object.keys(dataConf).reduce((a, k) => {
+                    a.push(`${k}=${encodeURIComponent(dataConf[k])}`);
+                    return a;
+                }, []).join('&');
+                const url = `${document.location.href}&${queryString}`;
+                axios.get(url).then((res) => {
+                    this.renderPreview = res.data.data;
+                })
+            }
+        },
+        components: {
+            draggable
+        }
+    };
 </script>
+
+<style lang="scss">
+	#renderPreview {
+		height: 100px;
+
+		div {
+			height: 50px;
+			border: solid 1px black;
+
+		}
+	}
+
+	.pagebuilder_container {
+		width: 100%;
+	}
+
+	#fieldset-pagebuilder div {
+
+	}
+
+	.sidebar .closeButton {
+		float: right;
+	}
+
+	.sidebar .sidebar-content {
+		margin-left: 15px;
+		margin-top: 12.5px
+	}
+</style>
--- build/media_source/system/js/fields/pagebuilder/components/settings/editelement.vue
+++ build/media_source/system/js/fields/pagebuilder/components/settings/editelement.vue
@@ -1,190 +1,232 @@
 <template>
-    <div>
-        <!-- Settings for editing elements -->
-        <fieldset>
-            <legend>{{ translate('JLIB_PAGEBUILDER_EDIT') }}{{ elementSelected.type }}</legend>
-            <div class="form-group">
-                <label for="element_class">{{ translate('JLIB_PAGEBUILDER_ADD_CLASS') }}</label>
-                <input type="text" name="element_class" id="element_class" class="form-control" :placeholder="translate('JLIB_PAGEBUILDER_NONE')" v-model="element_class">
+	<div>
+		<!-- Settings for editing elements -->
+		<div>
+			<legend>{{ translate('JLIB_PAGEBUILDER_EDIT') }}{{ elementSelected.type }}</legend>
+			<div class="form-group">
+				<label for="element_class">{{ translate('JLIB_PAGEBUILDER_ADD_CLASS') }}</label>
+				<div class="">
+					<input type="text" name="element_class" id="element_class" class="class_input"
+						   :placeholder="translate('JLIB_PAGEBUILDER_NONE')" v-model="element_class">
+					<span class="fa fa-plus add_class_button hoverCursor" @click="add"
+						  title="Add" aria-hidden="true"></span>
+				</div>
 			</div>
 
 			<div class="form-group">
-                <div v-if="elementSelected.type === 'column'">
-                    <label for="element_offset">{{ translate('JLIB_PAGEBUILDER_ADD_OFFSET') }}</label>
-                    <br>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Device</th>
-                                <th scope="col">Offset</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="size in offset_sizes">
-                                <th scope="row"><i :class="['fas', 'fa-' + size.icon, 'fa-lg', 'fa-rotate-' + size.rotate]"></i></th>
-                                <td class="control-group">
-                                    <select name="element_offset" id="element_offset" class="custom-select custom-select-sm" v-model="element_offset[size.label]">
-                                        <option v-for="opt in offset" v-if="opt.value <= threshold" :value="opt.value">
-                                            {{opt.label}}
-                                        </option>
-                                    </select>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div v-for="(config, id) in configs" class="form-group">
-                    <label :for="id">{{ config.label }}</label>
-                    <select v-if="config.type === 'select'" :id="id" :name="id" class="custom-select"
-                            :required="config.required" v-model="elementSelected.options[id]">
-                        <option v-for="(value, key) in config.value" :value="value">{{ key }}</option>
-                    </select>
-                    <input v-else :id="id" @name="id" :type="config.type" class="form-control"
-                            :required="config.required" v-model="elementSelected.options[id]" />
-			    </div>
+				<div v-if="elementSelected.type === 'column'">
+					<label for="element_offset">{{ translate('JLIB_PAGEBUILDER_ADD_OFFSET') }}</label>
+					<br>
+					<table class="table">
+						<thead>
+						<tr>
+							<th scope="col">{{ translate('JLIB_PAGEBUILDER_DEVICE') }}</th>
+							<th scope="col">{{ translate('JLIB_PAGEBUILDER_OFFSET') }}</th>
+						</tr>
+						</thead>
+						<tbody>
+						<tr v-for="size in offset_sizes">
+							<th scope="row"><i :class="['fas', 'fa-' + size.icon, 'fa-lg', 'fa-rotate-' + size.rotate]"
+											   class="icons_pictures"></i></th>
+							<td class="control-group">
+								<select name="element_offset" id="element_offset" class="custom-select custom-select-sm"
+										v-model="element_offset[size.label]">
+									<option v-for="opt in offset" v-if="opt.value <= threshold" :value="opt.value">
+										{{opt.label}}
+									</option>
+								</select>
+							</td>
+						</tr>
+						</tbody>
+					</table>
+				</div>
+				<div v-for="(config, id) in configs" class="form-group">
+					<label :for="id">{{ config.label }}</label>
+					<select v-if="config.type === 'select'" :id="id" :name="id" class="custom-select"
+							:required="config.required" v-model="elementSelected.options[id]">
+						<option v-for="(value, key) in config.value" :value="value">{{ key }}</option>
+					</select>
+					<input v-else :id="id" @name="id" :type="config.type" class="form-control"
+						   :required="config.required" v-model="elementSelected.options[id]"/>
+				</div>
 			</div>
-            
-            <div>
-                <button type="button" class="btn btn-success" @click="add">{{ translate('JLIB_PAGEBUILDER_ADD') }}</button>
-                <button type="button" class="btn btn-secondary" @click="closeNav">{{ translate('JTOOLBAR_CLOSE') }}</button>
-            </div>
-        </fieldset>
-    </div>
+
+			<div class="buttonsOnBottom">
+				<button type="button" class="btn btn-secondary" @click="closeNav">{{ translate('JTOOLBAR_CLOSE') }}
+				</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
+    import {mapState, mapMutations} from 'vuex';
 
-export default {
-    name: 'edit-element',
-    computed: {
-        ...mapState([
-            'activeDevice',
-            'elementSelected',
-            'elements',
-            'parent',
-            'size'
-        ]),
-        threshold() {
-            return this.size - this.elementSelected.options.size[this.activeDevice];
-        },
-        configs() {
-          return this.elements.find(el => el.id === this.elementSelected.type).config;
-        },
-    },
-    mounted() {
-        this.element_class = this.elementSelected.options.class;
-        if(this.elementSelected.options.offset)
-            this.element_offset = this.elementSelected.options.offset;
-    },
-    watch: {
-        elementSelected: {
-            deep: true,
-            handler() {
-                this.element_class = this.elementSelected.options.class;
-                if(this.elementSelected.options.offset)
-                    this.element_offset = this.elementSelected.options.offset;
+    export default {
+        name: 'edit-element',
+        computed: {
+            ...mapState([
+                'activeDevice',
+                'elementSelected',
+                'elements',
+                'parent',
+                'size'
+            ]),
+            threshold() {
+                return this.size - this.elementSelected.options.size[this.activeDevice];
             },
-        }
-    },
-    data() {
-        return {
-            element_class: '',
-            element_offset: {},
-            offset_sizes: [
-                {
-                    icon: 'mobile-alt',
-                    label: 'xs',
-                    rotate: '0'
+            configs() {
+                return this.elements.find(el => el.id === this.elementSelected.type).config;
+            },
+        },
+        mounted() {
+            this.element_class = this.elementSelected.options.class;
+            if (this.elementSelected.options.offset)
+                this.element_offset = this.elementSelected.options.offset;
+        },
+        watch: {
+            elementSelected: {
+                deep: true,
+                handler() {
+                    this.element_class = this.elementSelected.options.class;
+                    if (this.elementSelected.options.offset)
+                        this.element_offset = this.elementSelected.options.offset;
                 },
-                {
-                    icon: 'tablet-alt',
-                    label: 'sm',
-                    rotate: '0'
-                },
-                {
-                    icon: 'tablet-alt',
-                    label: 'md',
-                    rotate: '270'
-                },
-                {
-                    icon: 'laptop',
-                    label: 'lg',
-                    rotate: '0'
-                },
-                {
-                    icon: 'desktop',
-                    label: 'xl',
-                    rotate: '0'
-                },
-            ],
-            offset: [
-                {
-                    value: 0,
-                    label: 'No offset'
-                },
-                {
-                    value: 1,
-                    label: '1 column - 1/12'
-                },
-                {
-                    value: 2,
-                    label: '2 columns - 1/6'
-                },
-                {
-                    value: 3,
-                    label: '3 columns - 1/4'
-                },
-                {
-                    value: 4,
-                    label: '4 columns - 1/3'
-                },
-                {
-                    value: 5,
-                    label: '5 columns - 5/12'
-                },
-                {
-                    value: 6,
-                    label: '6 columns - 1/2'
-                },
-                {
-                    value: 7,
-                    label: '7 columns - 7/12'
-                },
-                {
-                    value: 8,
-                    label: '8 columns - 2/3'
-                },
-                {
-                    value: 9,
-                    label: '9 columns - 3/4'
-                },
-                {
-                    value: 10,
-                    label: '10 columns - 5/6'
-                },
-                {
-                    value: 11,
-                    label: '11 columns - 11/12'
-                },
-                {
-                    value: 12,
-                    label: '12 columns - 1/1'
-                }
-            ]
-        }
-    },
-    methods: {
-        ...mapMutations([
-            'closeNav',
-            'modifyElement'
-        ]),
-        add() {
-            let modify = {};
-            modify.class = (this.element_class !== '') ?  this.element_class : '';
-            modify.offset = this.element_offset;
-            this.modifyElement(modify);
-        }
-    },
-}
+            }
+        },
+        data() {
+            return {
+                element_class: {
+                    name: '',
+
+				},
+                element_offset: {},
+                offset_sizes: [
+                    {
+                        icon: 'mobile-alt',
+                        label: 'xs',
+                        rotate: '0'
+                    },
+                    {
+                        icon: 'tablet-alt',
+                        label: 'sm',
+                        rotate: '0'
+                    },
+                    {
+                        icon: 'tablet-alt',
+                        label: 'md',
+                        rotate: '270'
+                    },
+                    {
+                        icon: 'laptop',
+                        label: 'lg',
+                        rotate: '0'
+                    },
+                    {
+                        icon: 'desktop',
+                        label: 'xl',
+                        rotate: '0'
+                    },
+                ],
+                offset: [
+                    {
+                        value: 0,
+                        label: 'No offset'
+                    },
+                    {
+                        value: 1,
+                        label: '1 column - 1/12'
+                    },
+                    {
+                        value: 2,
+                        label: '2 columns - 1/6'
+                    },
+                    {
+                        value: 3,
+                        label: '3 columns - 1/4'
+                    },
+                    {
+                        value: 4,
+                        label: '4 columns - 1/3'
+                    },
+                    {
+                        value: 5,
+                        label: '5 columns - 5/12'
+                    },
+                    {
+                        value: 6,
+                        label: '6 columns - 1/2'
+                    },
+                    {
+                        value: 7,
+                        label: '7 columns - 7/12'
+                    },
+                    {
+                        value: 8,
+                        label: '8 columns - 2/3'
+                    },
+                    {
+                        value: 9,
+                        label: '9 columns - 3/4'
+                    },
+                    {
+                        value: 10,
+                        label: '10 columns - 5/6'
+                    },
+                    {
+                        value: 11,
+                        label: '11 columns - 11/12'
+                    },
+                    {
+                        value: 12,
+                        label: '12 columns - 1/1'
+                    }
+                ]
+            }
+        },
+        methods: {
+            ...mapMutations([
+                'closeNav',
+                'modifyElement'
+            ]),
+            add() {
+                let modify = {};
+                modify.class = (this.element_class !== '') ? this.element_class : '';
+                modify.offset = this.element_offset;
+                this.modifyElement(modify);
+            }
+        },
+    }
 </script>
+
+<style lang="scss">
+
+	.fa-rotate-270 {
+		margin-top: 0px;
+	}
+
+	.icons_pictures {
+		margin-top: 15px;
+	}
+
+	.buttonsOnBottom {
+		float: right;
+	}
+
+	.add_class_div {
+		display: inline;
+	}
+
+	.class_input {
+		padding: 5px;
+	}
+
+	.add_class_button {
+		margin-left: 15px;
+	}
+
+	.hoverCursor:hover {
+		cursor: pointer;
+	}
+
+</style>
